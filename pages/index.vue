6a76a9351f7a9be9751acec1387cbd2417a73c59
kanemototomoki <kanemototomoki@gmail.com>
2020-06-02 22:28:23 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,34 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-ssr-gh-page
-      </h1>
-      <h2 class="subtitle">
-        My grand Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <nav id="nav">
+    <nuxt-link to="/fetch-json">Fetch-JSON</nuxt-link> |
+    <nuxt-link to="/promise-animation">Promise-Animation</nuxt-link>
+  </nav>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import FetchJson from '~/components/FetchJson.vue'
+import PromiseAnimation from '~/components/PromiseAnimation.vue'
 
 export default {
   components: {
-    Logo
+    FetchJson,
+    PromiseAnimation
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+
+#nav {
+  padding: 30px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+#nav a .router-link-exact-active {
+  color: #42b983;
 }
 </style>
